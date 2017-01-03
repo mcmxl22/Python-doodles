9214c0ed10db5ec575f026652be1f4cfdeddea39
Micah M <mcmxl@reagan.com>
2017-01-03 13:36:52 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,13 @@
 import webbrowser
 
-new = 2
 
-choice = raw_input('Type site name:\n> ')
-site = choice
-url1 = 'www..com'
+def websites():
+    new = 2
+    choice = raw_input('Type site name:\n> ')
+    site = choice
+    url1 = 'www..com'
+    url = 'http://' + url1[:4] + site + url1[4:]
+    webbrowser.open(url, new)
 
-url = 'http://' +  url1[:4] + site + url1[4:]
-
-webbrowser.open(url,new=new)
+if __name__ == "__main__":
+    websites()
