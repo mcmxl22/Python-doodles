d56d858ecb7c10658b487ff6c7c517e7374be4f1
Micah M <mcmxl@reagan.com>
2017-01-03 13:33:43 -0700

    Update house.py

--- house.py
+++ house.py
@@ -1,83 +1,91 @@
 import subprocess
+import sys
+
 
 def front_porch():
-    print '\nYou\'re on the porch. Choose a door.\n'
-	
-    dic = {'front', 'exit'}
-    for d in dic:
-        print '%s' % d
-		
+    print ('\nYou\'re on the porch. Choose a door.\n')
+
+    portal = ('front', 'exit')
+    for i in portal:
+        print ('%s' % i)
+
     choice = raw_input('\n> ')
-	
-    if choice == 'exit':
+
+    if choice == portal[1]:
         quit()
-			
-    elif choice == 'front':
+
+    elif choice == portal[0]:
         living_room()
 
+
 def stairs():
-    print '\nYou\'re in the upstairs hall. There are 6 doors. Choose one.'
-	
+    print ('\nYou\'re in the upstairs hall. There are 6 doors. Choose one.')
+
     doors = [range(1, 8)]
-	
-    for d in doors:
-        print '\n%s' % d
-      
+
+    for i in doors:
+        print ('\n%s' % i)
+
     choice = raw_input('\n> ')
-    
+
     if choice in {'1', '2', '3', '5'}:
-        print 'This room is occupied. The door is locked.'
+        print ('This room is occupied. The door is locked.')
         stairs()
 
     elif choice in {'4', '6'}:
-        print 'This room is empty.'
+        print ('This room is empty.')
         stairs()
-		
+
     elif choice == '7':
         living_room()
-		
+
+
 def living_room():
-    print '\nYou\'re in the livingroom. Choose a room or activity.\n'
-	
-    list = ('kitchen', 'stairs', 'porch', 'basement', 'browse', 'rest')
-	
-    for l in list:
-        print '%s' % l
-		
+
+    web = [sys.executable, 'web.py']
+	count = [sys.executable, 'countd.py']
+
+    print ('\nYou\'re in the livingroom. Choose a room or activity.\n')
+
+    room = ('kitchen', 'stairs', 'porch', 'basement', 'browse', 'rest')
+
+    for i in room:
+        print ('%s' % i)
+
     choice = raw_input('\n> ')
-	
-    if choice == list[1]:
+
+    if choice == room[1]:
         stairs()
-		
-    elif choice == list[2]:
+
+    elif choice == room[2]:
         front_porch()
-		
-    elif choice == list[0]:
-        print 'The kitchen is being remodeled. Come back later.'
+
+    elif choice == room[0]:
+        print ('The kitchen is being remodeled. Come back later.')
         living_room()
-		
-    elif choice == list[3]:
-        print 'Do you want to do laundry?'
-		
-        list = ('yes', 'no')
-		
-        for l in list:
-            print '\n%s' % l
+
+    elif choice == room[3]:
+        print ('Do you want to do laundry?')
+
+        answer = ('yes', 'no')
+
+        for i in answer:
+            print ('\n%s' % i)
+
         choice = raw_input('\n> ')
-        if choice == list[0]:
-            print '\nYou\'ll have to wait.'
+        if choice == answer[0]:
+            print ('\nYou\'ll have to wait.')
             living_room()
-        elif choice == list[1]:
+        elif choice == answer[1]:
             living_room()
-		
-    elif choice == list[4]:
-        subprocess.call('web.py', shell=True)
+
+    elif choice == room[4]:
+        subprocess.call(web)
         living_room()
-	
-    elif choice == list[5]:
-        print list[5]+'ing'
-        subprocess.call('countd.py', shell=True)
+
+    elif choice == room[5]:
+        subprocess.call(count)
         living_room()
-		
+
 if __name__ == "__main__":
     front_porch()
