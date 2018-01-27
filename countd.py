e32c6e5837e0403c19bd443893a4ae2c3d72ec71
Micah M <mcmxl@reagan.com>
2018-01-27 13:35:11 -0700

    Update countd.py

--- countd.py
+++ countd.py
@@ -3,7 +3,7 @@
 import time
 
 def count():
-    seconds = int(input('How long? \n> '))
+    seconds = int(input('How long? '))
     for i in reversed(range(seconds)):
         time.sleep(1)
         print ("%s\r" % i),
