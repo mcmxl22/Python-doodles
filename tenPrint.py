c354db1de2dfb103e18e96c9ca0f869816d548d7
Micah M <mcmxl@reagan.com>
2018-01-26 19:15:39 -0700

    Update tenPrint.py

--- tenPrint.py
+++ tenPrint.py
@@ -1,6 +1,9 @@
 import random
+
 slash1 = '\\'
 slash2 = '/'
+slash3 = '|'
+slash4 = '-'
 
 while True:
-    print random.choice([slash1, slash2]),
+    print random.choice([slash1, slash2, slash3, slash4]),
