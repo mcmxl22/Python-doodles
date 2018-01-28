f7a1a1ba4dc60964a0d1776953605504a6b22f2a
Micah M <mcmxl@reagan.com>
2018-01-28 16:40:00 -0700

    Update and rename countd.py to countDown.py

--- countDown.py
+++ countDown.py
@@ -1,8 +1,14 @@
-# By Micah M. 2017
+#! /usr/bin/env python
+# By Micah M. 2018
+# countDown version 1.0
+# Python 2.7.13
+
 
 import time
 
+
 def count():
+
     seconds = int(input('How long? '))
     for i in reversed(range(seconds)):
         time.sleep(1)
