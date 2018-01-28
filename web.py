9e0ac5be5591e77a50ecf4f9df847a187185f5ca
Micah M <mcmxl@reagan.com>
2018-01-28 16:38:34 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,14 @@
+#! /usr/bin/env python
+# By Micah M. 2018
+# web version 1.0
+# Python 2.7.13
+
+
 import webbrowser
 
 
 def websites():
+
     new = 2
     choice = raw_input('Type site name:\n> ')
     site = choice
