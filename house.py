c605549df0eb0a227b88a85748d112bc6aac904e
Micah M <mcmxl@reagan.com>
2018-01-28 15:28:50 -0700

    Update house.py

--- house.py
+++ house.py
@@ -8,15 +8,13 @@ import sys
 
 
 def front_porch():
+    
     print ('\nYou\'re on the porch. Choose a door.\n')
-
     portal = ('front', 'exit')
-
     for i in portal:
         print ('%s' % i)
 
     choice = raw_input('\n> ')
-
     if choice == portal[0]:
         living_room()
 
@@ -25,15 +23,13 @@ def front_porch():
 
 
 def stairs():
+    
     print ('\nYou\'re in the upstairs hall. There are 6 doors. Choose one.')
-
     doors = [range(1, 8)]
-
     for i in doors:
         print ('\n%s' % i)
 
     choice = raw_input('\n> ')
-
     if choice in {'1', '2', '3', '5'}:
         print ('This room is occupied. The door is locked.')
         stairs()
@@ -47,20 +43,19 @@ def stairs():
 
 
 def kitchen():
+    
     print ('The kitchen is being remodeled. Come back later.')
     living_room()
 
 
 def basement():
+    
     print ('Do you want to do laundry?')
-
     answer = ('yes', 'no')
-
     for i in answer:
         print ('\n%s' % i)
 
     choice = raw_input('\n> ')
-
     if choice == answer[0]:
         print ('\nYou\'ll have to wait.')
         living_room()
@@ -70,17 +65,14 @@ def basement():
 
 
 def living_room():
+    
     print ('\nYou\'re in the livingroom. Choose a room or activity.\n')
-
     web, count = [sys.executable, 'web.py'], [sys.executable, 'countd.py']
-
     room = ('kitchen', 'stairs', 'porch', 'basement', 'browse', 'rest')
-
     for i in room:
         print ('%s' % i)
 
     choice = raw_input('\n> ')
-
     if choice == room[1]:
         stairs()
 
