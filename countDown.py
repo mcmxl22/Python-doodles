44307b95f6104031fc51aa114a9a46acb0715c70
Micah M <mcmxl@reagan.com>
2018-01-28 18:27:12 -0700

    Update countDown.py
    Python 3.X compatible.

--- countDown.py
+++ countDown.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python
 # By Micah M. 2018
-# countDown version 1.0
-# Python 2.7.13
+# countDown version 1.01
+# Python 3.6.4
 
 
 import time
 
 
 def count():
-
     seconds = int(input('How long? '))
     for i in reversed(range(seconds)):
         time.sleep(1)
         print ("%s\r" % i),
 
+
 if __name__ == "__main__":
-  count()
+    count()
