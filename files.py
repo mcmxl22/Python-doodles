9aa51ef2de370e396f87c01dc58a6c86d0b777d8
Micah M <mcmxl@reagan.com>
2018-01-28 18:46:22 -0700

    Update files.py
    Python 3.X compatible.

--- files.py
+++ files.py
@@ -1,6 +1,7 @@
-#By Micah M. 2017 
-#Files Version 1.0            
-#Python 2.7.12
+#! /usr/bin/env python
+# By Micah M. 2018
+# Files Version 1.01
+# Python 3.6.4
 
 
 import os.path
@@ -8,10 +9,10 @@ import os.path
 
 class Files(object):
     #Create a file and confirm it.
-    def create(self):
+    def create():
         fi = Files()
         #create file.
-        filename = raw_input('Enter file name. ')
+        filename = input('Enter file name. ')
         if os.path.exists(filename) == True:
             print ('File already exists!')
             fi.create()
@@ -21,47 +22,11 @@ class Files(object):
         if os.path.exists(filename) == True:
             print ('Done!')
         f.close()
-        main()
 		
-	#Edit a file.
-    def edit(self):
-        fi = Files()
-        FileOpen = raw_input('Enter file name. ')
-        f = open(FileOpen, 'w')
-        FileEdit = raw_input('> ')
-        f.write(FileEdit)
-        #...
-        f.close()
-        print ('Done!')
-        main()
-        
-class questions(object):
-
-    def start(self):
-
-        Start_Actions = ('1 Create file',
-        '2 Edit file', '3 Exit')
-
-        for i in Start_Actions:
-            print ('%s' % i)
-        Ask = raw_input('Choose an option. ')
-        f = Files()
-        if Ask == '1':
-            f.create()
-
-        elif Ask == '2':
-            f.edit()
-
-        elif Ask == '3':
-           quit()
-
-        else:
-            print ('Invalid entry!')
-            main()
-
 class main(object):
     def __init__(self):
-        q = questions()
-        q.start()   
-
-main()
+        files = Files()
+        Files.create()		
+		
+if __name__ == "__main__":
+    main()
