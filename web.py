5626910146fcf5f63b1f0eb2c5a5f357e7aac2be
Micah M <mcmxl@reagan.com>
2018-01-29 01:03:02 -0700

    Update web.py
    Python 3.X compatible.

--- web.py
+++ web.py
@@ -1,20 +1,20 @@
 #! /usr/bin/env python
-# By Micah M. 2018
-# web version 1.0
-# Python 2.7.13
+# By Micah M. 2017
+# web version 1.01
+# Python 3.6.4
 
 
 import webbrowser
 
 
 def websites():
-
     new = 2
-    choice = raw_input('Type site name:\n> ')
+    choice = input('Type site name:\n> ')
     site = choice
     url1 = 'www..com'
     url = 'http://' + url1[:4] + site + url1[4:]
     webbrowser.open(url, new)
+
 
 if __name__ == "__main__":
     websites()
