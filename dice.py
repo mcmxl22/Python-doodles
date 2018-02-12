db1d25de016b2a3d20922142240f7c46c279d6ea
Micah M <mcmxl@reagan.com>
2018-02-11 23:46:03 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,26 +1,19 @@
 #! /usr/bin/env python
 # By Micah M. 2018
-# dice version 1.0
+# dice version 1.0.01
 # Python 3.6.4
 
 
 import random
-
-
+ 
 def dice():
-    sides = [1, 2, 3, 4, 5, 6]
-    rollDice = input('roll? ')
-	
-    if rollDice == 'y':
-        i = random.choice(sides)
-        print(i)
-        dice()
-
-    elif rollDice == 'n':
-        quit()
-    
-    else:
-        print('invalid entry')
-        dice()
-
+    while True:
+        roll_dice = input('Roll? ')
+        if roll_dice in ['y', '']:
+            print(random.randint(1, 6))
+        elif roll_dice == 'n':
+            raise SystemExit
+        else:
+            print('invalid entry')
+ 
 dice()
