396dff86387c61c264ea99bfd195e863b56b79ab
Micah M <mcmxl@reagan.com>
2018-07-29 01:39:09 -0600

    Add files via upload

---
+++ tempConvert.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# By Micah M. 2018
+# tempConvert version 1
+# Python 3.7
+
+
+celsius = input('Enter the temperature in Celsius.\n>')
+temp = int(celsius) * 1.8 + 32
+print(f'{celsius} degrees Celsius is {temp} degrees Fahrenheit.')
+ 
