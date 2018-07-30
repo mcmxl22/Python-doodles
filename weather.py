d383eb683062f59560b4fa6da9ca9d3065e64191
Micah M <mcmxl@reagan.com>
2018-07-29 20:27:09 -0600

    Update and rename tempConvert.py to weather.py
    Changed name and added functionality.

--- weather.py
+++ weather.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 # By Micah M. 2018
-# tempConvert version 1.2
+# weather version 1.2
 # Python 3.7
 
 
 
-#class Conversion(object):
+def dewPoint():
+    t = input('\nEnter the temperature in Celsius.\n> ')
+    rh = input ('\nEnter the relative humidity\n> ')
+    formula = (int(t) - ((100 - int(rh)) / 5))
+    print(f'\nThe dew point is {int(formula)} degrees Celsius.\n')
+
 def celsius():
     convert = input('\nEnter the temperature in Celsius.\n> ')
     formula = int(convert) * 1.8 + 32
@@ -15,25 +20,21 @@ def fahrenheit():
     convert = input('\nEnter the temperature in Fahrenheit.\n> ')
     formula = (int(convert) - 32) / 1.8
     print(f'{convert} degrees Fahrenheit is {int(formula)} degrees Celsius.\n')
-		
-#class Main(object):
-def prompt():
 
-    	while True:
-            options = ('1 Fahrenheit', '2 Celsius', '3 Exit')
+def prompt():
+    	while True:    
+            options = ('1 Convert from Fahrenheit', '2 Convert from Celsius', '3 Find Dew Point', '4 Exit')
             print('\n'.join(options))
-            unitChoice = input('\nWhat unit would you like to convert from?\n> ')
+            unitChoice = input('\nWhat would you like to do?\n> ')
             if unitChoice == '1':
                 fahrenheit()
             elif unitChoice == '2':
                 celsius()
             elif unitChoice == '3':
+                dewPoint()
+            elif unitChoice == '4':
                 raise SystemExit
             else:
-                print('Invalid Entry')
+                print('\nInvalid Entry\n')
 
-
-
-#if __name__ == "__main__":
-#m = Main()
 prompt()
