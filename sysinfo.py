b963f6690fda0b358df065ba42070eff4981068e
Micah M <mcmxl@reagan.com>
2018-07-31 18:50:03 -0600

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -1,7 +1,17 @@
+#! /usr/bin/env python
+# By Micah M. 2018
+# sysinfo version 1.0
+# Python 3.7
+
 import platform
 import psutil
 
-sysinfo = platform.uname()
-meminfo = psutil.virtual_memory()
-print(sysinfo)
-print(meminfo)
+
+def sysinfo():
+    sysinfo = platform.uname()
+    meminfo = psutil.virtual_memory()
+    print(sysinfo)
+    print(meminfo)
+
+if __name__ == "__main__":
+    sysinfo()
