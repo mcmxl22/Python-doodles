aa5bc785b022963e7f5cf93bae4ef9c75585386d
Micah M <mcmxl@reagan.com>
2018-08-26 20:13:17 -0600

    minor changes

--- house.py
+++ house.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # By Micah M. 2018
-# House version 1.01.03
-# Python 3.6.4
+# House version 1.02
+# Python 3.7
 
 
 import subprocess
@@ -9,23 +9,24 @@ import sys
 from time import sleep
 
 
+'''This program requires web.py and countDown.py'''
 def Porch():
 
     while True:
-        choice = input('\nChoose a door. \n1 Front \n2 Exit \n> ')
+        choice = input('\nChoose a door. \n1 front \n2 exit \n>')
         if choice == '1':
             livingRoom()
         elif choice == '2':
             raise SystemExit
         else:
-            print('Invalid Answer.')
+            print('That is not a valid answer.')
 
 
 def stairs():
 
     while True:
         for i in range(1, 7):
-            print('%s' % i)
+            print(f'{i}')
         choice = input('''\nYou\'re in the upstairs hall.
                        \rThere are 6 doors. Choose one. > ''')
         if choice in {'1', '2', '3', '5'}:
@@ -45,19 +46,18 @@ def kitchen():
 def basement():
 
     while True:
-        laundry = input('Do you want to do laundry? \n1 Yes \n2 No \n> ')
+        laundry = input('Do you want to do laundry? \n1 yes, \n2 no \n> ')
         if laundry == '1':
             quarters = int(input('How many quarters do you have? '))
             if quarters < 8:
                 print('You need more money.')
             elif quarters >= 8:
-                while True:
-                    print('Washing')
-                    sleep(5)
-                    print('Drying')
-                    sleep(10)
-                    print('Done')
-                    livingRoom()
+                print('washing')
+                sleep(5)
+                print('drying')
+                sleep(10)
+                print('done')
+                livingRoom()
         else:
             livingRoom()
 
@@ -66,11 +66,10 @@ def livingRoom():
 
     while True:
         roomSelect = ('1 Kitchen', '2 Stairs', '3 Porch', '4 Basement',
-        '5 Browse', '6 Rest')
-        for room in roomSelect:
-            print('%s' % room)
+                      '5 Browse', '6 Rest')
+        print(' \n'.join(roomSelect))
         room = input('''\nYou\'re in the living room.
-                     \rChoose a room or activity. \n> ''')
+                     \rChoose a room or activity. > ''')
         web = [sys.executable, 'web.py']
         count = [sys.executable, 'countDown.py']
         if room == '1':
