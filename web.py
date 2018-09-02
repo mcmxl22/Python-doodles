726df9023928f6ec5670016cd349e2cc0aaf15b2
Micah M <mcmxl@reagan.com>
2018-09-01 23:30:44 -0600

    Update web.py

--- web.py
+++ web.py
@@ -1,19 +1,18 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
 # By Micah M. 2018
-# Web version 1.01
-# Python 3.6.4
+# Web version 1.2
+# Python 3.7
 
 
 import webbrowser
 
 
-def websites():
-    new = 2
-    choice = input('Type site name:\n> ')
-    url1 = 'www..com'
-    url = 'http://' + url1[:4] + choice + url1[4:]
-    webbrowser.open(url, new)
+def website():
+
+    site = input('Type site name: ')
+    url = f'http://www.{site}.com'
+    webbrowser.open(url)
 
 
 if __name__ == "__main__":
-    websites()
+    website()
