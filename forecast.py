1d8dcdcd872fca2f0f0cb7c6b52eeb36f1345572
Micah M <mcmxl@reagan.com>
2018-09-22 18:07:09 -0600

    Add files via upload

---
+++ forecast.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''By Micah M. 2018
+   forecast version 1.1
+   Python 3.7'''
+
+def forecast():
+    '''Forecasts based on barometric pressure trends
+       and logs which trend is chosen.'''
+    trendOptions = ['1 Rising', '2 Falling', '3 Steady']
+    print('\n'.join(trendOptions))
+    trend = input('\nChoose a trend.\n> ')
+    # logEntry = trend  # Logs chosen option.
+    # file = open('trendLog.txt', 'a')
+    # file.write(logEntry)  # Writes log to file.
+    # rLog = open('trendLog.txt').read()  # Reads log from file.
+    # mostCommon = max(rLog, key=rLog.count) # Finds most common log entry.
+    # file.close()
+    # print(f'\nYour most common choice: {mostCommon}.')
+    if trend == '1':
+        print('\nFairer weather on the way.\n')
+    elif trend == '2':
+        print('\nPoorer weather on the way.\n')
+    elif trend == '3':
+        print('\nNo significant change.\n')
+    else:
+        print('Invalid Entry!\n')
+        forecast()
+
+
+if __name__ == "__main__":
+    forecast()
