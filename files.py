dadf76353f946a6a2574944ec81731d72df00c67
Micah M <mcmxl@reagan.com>
2018-10-15 11:01:54 -0600

    Update files.py

--- files.py
+++ files.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 '''By Micah M. 2018
    Files Version 1.3
    Python 3.7'''
-
 
 import os.path
 
 
 def files():
-'''Create a file and/or confirm it.'''
-    fileName = input('Enter file name.\n> ')
-    if os.path.exists(fileName) is True:
-        print(f'File {fileName} already exists!')
-        Files.create()
-    f = open(fileName, 'w+')
-    print('Creating file!')
-    if os.path.exists(fileName) is True:
-        print('Done!')
-    f.close()
+    '''Create a file and/or confirm it.'''
+    while True:
+        file_name = input('Enter file name.\n> ')
+        if os.path.exists(file_name) is True:
+            print(f'{file_name} already exists!')
+        else:
+            f = open(file_name, 'w+')
+            print('Creating file!')
+            if os.path.exists(file_name) is True:
+                print('Done!')
+            f.close()
 
 
 if __name__ == "__main__":
