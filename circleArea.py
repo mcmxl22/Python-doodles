f17d789d99dc967cf3f808e68233381b0630762a
Micah M <mcmxl@reagan.com>
2018-10-18 02:46:47 -0600

    Update circleArea.py

--- circleArea.py
+++ circleArea.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 '''By Micah M. 2018
    circleArea version 1.1
    Python 3.7'''
@@ -7,10 +7,11 @@
 import math
 
 
-def area():
+def circle_area():
+   '''Find the area of a circle'''
     while True:
         radius = input('Enter the radius of the circle.\n> ')
         result = math.pi * int(radius)**2
         print(f'The area is {round(result, 2)}.')
 
-area()
+circle_area()
