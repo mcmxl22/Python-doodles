078d89acdb9507634389826cc34d9ddae226c2c2
Micah M <mcmxl@reagan.com>
2018-10-24 11:26:17 -0600

    Update house.py

--- house.py
+++ house.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 '''By Micah M. 2018
    House version 1.2
-   Python 3.7
-   Requires web.py and count_down.py'''
+   Python 3.7.1
+   This program requires web.py and countDown.py'''
 
 
 import subprocess
@@ -13,10 +13,12 @@ from time import sleep
 def porch():
     '''porch'''
     while True:
-        choice = input('\nChoose a door. \n1 front \n2 exit \n>')
-        if choice == '1':
+        porch_options = ['1 front', '2 exit ']
+        print(' \n'.join(porch_options))
+        porch_choice = input('Choose a option. ') 
+        if porch_choice in '1':
             living_room()
-        elif choice == '2':
+        elif porch_choice in '2':
             raise SystemExit
         else:
             print('Invalid answer.')
@@ -27,11 +29,11 @@ def stairs():
     while True:
         for i in range(1, 7):
             print(i)
-        choice = input('''\nYou\'re in the upstairs hall.
-                       \rChoose a door. > ''')
-        if choice in {'1', '2', '3', '5'}:
+        hall_choice = input('''\nYou\'re in the upstairs hall.
+                               \rChoose a door. ''')
+        if hall_choice in ['1', '2', '3', '5']:
             print('This door is locked.')
-        elif choice in {'4', '6'}:
+        elif hall_choice in ['4', '6']:
             print('This room is empty.')
         else:
             living_room()
@@ -46,8 +48,10 @@ def kitchen():
 def basement():
     '''basement'''
     while True:
-        laundry = input('Do you want to do laundry? \n1 yes, \n2 no \n> ')
-        if laundry == '1':
+        laundry_option = ['yes', 'No']
+        print(' \n'.join(laundry_option))
+        laundry = input('Do you want to do laundry? ')
+        if laundry in '1':
             quarters = int(input('How many quarters do you have? '))
             if quarters < 8:
                 print(f'{quarters} is not enough money.')
@@ -64,25 +68,25 @@ def basement():
 def living_room():
     '''living_room'''
     while True:
-        room_select = ('1 Kitchen', '2 Stairs', '3 Porch', '4 Basement',
-                       '5 Browse', '6 Rest')
+        room_select = ['1 Kitchen', '2 Stairs', '3 Porch', '4 Basement',
+                       '5 Browse', '6 Rest']
         print(' \n'.join(room_select))
         room = input('''\nYou\'re in the living room.
-                     \rChoose a room or activity. > ''')
+                        \rChoose a room or activity. ''')
         web = [sys.executable, 'web.py']
         count = [sys.executable, 'countDown.py']
-        if room == '1':
+        if room in '1':
             kitchen()
-        elif room == '2':
+        elif room in '2':
             stairs()
-        elif room == '3':
+        elif room in '3':
             porch()
-        elif room == '4':
+        elif room in '4':
             basement()
-        elif room == '5':
-            subprocess.run(web)
-        elif room == '6':
-            subprocess.run(count)
+        elif room in '5':
+            subprocess.call(web)
+        elif room in '6':
+            subprocess.call(count)
         else:
             print('Invalid Answer.')
 
