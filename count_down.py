2bda7dd2956954cb5d0291fa8015a74242a26bd4
Micah M <mcmxl@reagan.com>
2018-11-04 13:00:26 -0700

    Update count_down.py

--- count_down.py
+++ count_down.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 '''By Micah M. 2018
-   count_down version 1.1
-   Python 3.7'''
+   countDown version 1.1
+   Python 3.7.1'''
 
 
 import time
 
 
-def count_down():
+def count(seconds):
+    '''Count down in seconds.'''
     seconds = int(input('How long? '))
     for i in reversed(range(seconds)):
         time.sleep(1)
         print(i, end='\r')
 
 if __name__ == "__main__":
-    count_down()
+    count()
