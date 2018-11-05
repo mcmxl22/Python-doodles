0a8f9860f3d6970e878952b4a0555584431c20a3
Micah M <mcmxl@reagan.com>
2018-11-04 23:01:22 -0700

    Update circle_area.py

--- circle_area.py
+++ circle_area.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 '''By Micah M. 2018
-   circle_area version 1.1
-   Python 3.7'''
+   circle_area version 1.2
+   Python 3.7.1'''
 
 
 import math
 
 
-def circle_area():
+def find_area(radius):
     '''Find the area of a user defind circle.'''
     while True:
-        radius = input('Enter the radius of the circle.\n> ')
+        radius = input('Enter the radius of the circle. ')
         result = math.pi * int(radius)**2
-        print(f'The area is {round(result, 2)}.')
+        if radius == int():
+            print(f'The area is {round(result, 2)}.')
+        elif radius == str:
+            print('Entry must be an intiger!')
 
-circle_area()
+
+if __name__ == "__main__":
+    find_area('radius')
