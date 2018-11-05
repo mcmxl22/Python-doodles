d6650206095bee4f1b19cead8e9abf8f5bef51a9
Micah M <mcmxl@reagan.com>
2018-11-05 11:53:37 -0700

    Update files.py

--- files.py
+++ files.py
@@ -1,24 +1,45 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python
 '''By Micah M. 2018
-   Files Version 1.3
-   Python 3.7'''
+   files Version 1.4
+   Python 3.7.1'''
 
 import os.path
 
 
-def files():
+def create_files(file_name):
     '''Create a file and/or confirm it.'''
+    file_name = input('Enter file name. ')
+    if os.path.exists(file_name):
+        print(f'{file_name} already exists!')
+    else:
+        f = open(file_name, 'w+')
+        if os.path.exists(file_name):
+            print(f'{file_name} created!')
+        f.close()
+
+
+def delete_file(file_name):
+    '''Delete files.'''
+    file_name = input('Enter file to be deleted. ')
+    confirm_file = input(f'Are you sure you want to delete {file_name}? ')
+    if confirm_file is 'y':
+        os.remove(file_name)
+        print(f'{file_name} deleted!')
+
+
+def file_options(file_name):
+    '''Choose what to do with a file.'''
     while True:
-        file_name = input('Enter file name.\n> ')
-        if os.path.exists(file_name) is True:
-            print(f'{file_name} already exists!')
-        else:
-            f = open(file_name, 'w+')
-            print('Creating file!')
-            if os.path.exists(file_name) is True:
-                print('Done!')
-            f.close()
+        file_options = ['1 Create file', '2 Delete file', '3 Exit']
+        print('\n'.join(file_options))
+        file_choice = input('Choose an option. ')
+        if file_choice in '1':
+            create_files(file_name)
+        elif file_choice in '2':
+            delete_file(file_name)
+        elif file_choice in '3':
+            raise SystemExit
 
 
 if __name__ == "__main__":
-    files()
+    file_options('file_name')
