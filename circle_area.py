9cc22f07db89800afe034d24afc9ff79bd994cd5
Micah M <mcmxl@reagan.com>
2019-03-30 16:29:03 -0600

    Update circle_area.py
    Replaced if/else with try/except. Replaced print() in find_area() with return. Moved print() to if __name__ == "__main__":.

--- circle_area.py
+++ circle_area.py
@@ -1,21 +1,25 @@
-#!/bin/python3
-"""circle_area version 1.2
+#!/usr/bin/env python3
+"""circle_area version 1.3
    Python 3.7.2"""
 
 
 import math
 
 
-def find_area(radius):
+def find_area():
     """Find the area of a user defined circle."""
     while True:
         radius = input('Enter the radius of the circle. ')
         result = math.pi * int(radius)**2
-        if radius == int():
-            print(f'The area is {round(result, 2)}.')
-        elif radius == str:
-            print('Entry must be an integer!')
+        
+        try:
+           number = int(radius) 
+        except ValueError as e:
+            print(f'Please enter a number.')
+        else:
+            return result
 
 
 if __name__ == "__main__":
-    find_area('radius')
+    area = find_area()
+    print(f'The area of the circle is {area}.')
