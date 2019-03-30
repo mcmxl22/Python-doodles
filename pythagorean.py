0ef7b323a437cea46cb81b01439124913b2cc41c
Micah M <mcmxl@reagan.com>
2019-03-30 16:22:07 -0600

    Update pythagorean.py
    Took the print() out of pythag() and put it under if __name__ == "__main__":.  Replaced print() with return.

--- pythagorean.py
+++ pythagorean.py
@@ -1,16 +1,17 @@
-#!/bin/python3
-"""pythagorean version 1.2
+#!/usr/bin/env python3
+"""Pythagorean version 1.3
    Python 3.7.2"""
 
 
 def pythag():
-    """find the length of side C
-       when A and B are known."""
+    """Find the length of side C
+       when sides A and B are known."""
     while True:
         a = input('Enter length a: ')
         b = input('Enter length b: ')
         c = int(a)**2 + int(b)**2
-        print(f'Length c is {c}.')
+        return c
 
 if __name__ == "__main__":
-    pythag()
+    pythag = pythag()
+    print(f'Length c is {pythag}.'
