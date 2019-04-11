799c61068056512cb4335b9e33ae606d9283c638
Micah M <mcmxl@reagan.com>
2019-04-11 13:33:58 -0600

    Update age100.py

--- age100.py
+++ age100.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 """age100 version 1.2
    Python 3.7.2"""
 
@@ -8,10 +8,11 @@ import datetime
 
 def age100():
     """The year you will be 100"""
-    get_name = input('What\'s your name? ')
-    get_age = int(input('What\'s your age? '))
+    get_name = input("What's your name? ")
+    get_age = int(input("What's your age? "))
     current_year = datetime.datetime.now()
     age_100 = (current_year.year - get_age) + 100
+
     if get_age >= 100:
         print('Congratulations! You already reached 100!')
     else:
