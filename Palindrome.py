608c32951be68cf207eb8520d2b33d810bf39684
Micah M <mcmxl@reagan.com>
2019-06-13 11:55:33 -0600

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-"""Palindrome version 1.2
-   Python 3.7.2"""
+"""
+Palindrome version 1.2
+Python 3.7.3
+"""
 
-word = input('Enter a word or phrase. ')
-letters = filter(str.strip, list(word))
 
-test = list(reversed(word))
-letter_test = filter(str.strip, test)
+def palindrome():
+    word = input('Enter a word or phrase. ')
+    letters = filter(str.strip, list(word))
 
-if list(letter_test) == list(letters):
-    print(f'{word} is a palindrome!')
+    test = list(reversed(word))
+    letter_test = filter(str.strip, test)
+
+    if list(letter_test) == list(letters):
+        print(f'{word} is a palindrome!')
+
 else:
     print(f'{word} is not a palindrome!')
+
+if __name__ == "__main__":
+    palindrome()
