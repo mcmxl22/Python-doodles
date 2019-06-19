25c1f2c8b31b8c2ed63b5023e18ec11fe30b24ac
Micah M <mcmxl@reagan.com>
2019-06-18 19:37:52 -0600

    Update and rename weird.py to fun/weird.py

--- fun/weird.py
+++ fun/weird.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-"""weird version 1
-   Python 3.7.2"""
+"""
+weird version 1
+Python 3.7
+"""
 
 
-def weird():
-    '''weird'''
+def is_weird():
+    '''Is a number odd or even?'''
     while True:
         pick_number = int(input('Pick a number: '))
+
         if pick_number % 2 == 0:
             print('Not Weird!')
+
         else:
             print('Weird!')
-
 
 weird()
