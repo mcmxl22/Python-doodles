8378351121fbfadeb1fd7fb2c2f7a0cd1da8fa28
Micah M <mcmxl@reagan.com>
2019-06-20 01:15:21 -0600

    Update The_Matrix.py

--- fun/The_Matrix.py
+++ fun/The_Matrix.py
@@ -1,18 +1,8 @@
 import random
  
-num0 = '0'
-num1 = '1'
-num2 = '2'
-num3 = '3'
-num4 = '4'
-num5 = '5'
-num6 = '6'
-num7 = '7'
-num8 = '8'
-num9 = '9'
+num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
  
 while True:
-    matrix = random.choice([num0, num1, num2, num3, num4,
-                            num5, num6, num7, num8, num9])
+    matrix = random.choice(num)
     for i in matrix:
         print(i, end='\t')
