18adf0307bed7f93a3cd3124f12a8008af3cf95a
Micah M <mcmxl@reagan.com>
2019-09-01 12:23:22 -0600

    Update pythagorean.py

--- maths/pythagorean.py
+++ maths/pythagorean.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 """
-Pythagorean version 1.3
+Pythagorean version 1.4
 Python 3.7
 """
+import math
 
 
 def pythag():
     """Find the length of side C
        when sides A and B are known."""
     while True:
-        a = input('Enter length a: ')
-        b = input('Enter length b: ')
-        c = int(a)**2 + int(b)**2
-        return c
+        length_a = input("Enter length a: ")
+        length_b = input("Enter length b: ")
+        length_c = math.sqrt(int(length_a) ** 2 + int(length_b) ** 2)
+        return f"Length c is {length_c}."
+
 
 if __name__ == "__main__":
     pythag = pythag()
-    print(f'Length c is {pythag}.')
+    print(pythag)
