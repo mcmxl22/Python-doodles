b809bcfba10910c9db4b578c7080cafcdcd6e06e
mcmxl22 <mcmxl@hotmail.com>
2019-09-13 01:23:38 -0600

    Minor changes.

--- maths/circle_area.py
+++ maths/circle_area.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-circle_area version 1.3
+circle_area version 1.4
 Python 3.7
 """
 
@@ -8,18 +8,18 @@ import math
 
 
 def find_area():
-    """Find the area of a user defined circle."""
+    """Find the area of a circle."""
     while True:
-        radius = input('Enter the radius of the circle. ')
-        result = math.pi * int(radius)**2
+        radius = input("Enter the radius of the circle. ")
+        result = math.pi * int(radius) ** 2
         try:
-           number = int(radius)
-        except ValueError as e:
-            print(f'Please enter a number.')
+            number = int(radius)
+        except ValueError as Error:
+            print(f"Please enter a number.")
         else:
             return result
 
 
 if __name__ == "__main__":
     area = find_area()
-    print(f'The area of the circle is {area}.')
+    print(f"The area of the circle is {area}.")
