98877775b46b0fbb81f84186ec0a813cb8fcda8b
mcmxl22 <mcmxl@hotmail.com>
2019-09-13 00:45:58 -0600

    Minor Changes.

--- fun/dice.py
+++ fun/dice.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-dice version 1.1
+dice version 1.2
 Python 3.7
 """
 
-
 import random
 
+
 def dice():
-   """Roll the dice"""
+    """Roll the dice"""
     while True:
         roll_dice = input('Roll? ')
 
-        if roll_dice in ['y', '']
+        if roll_dice in ['y', '']:
             print(random.randint(1, 6)
 
         elif roll_dice == 'n':
@@ -21,5 +21,6 @@ def dice():
         else:
             print('Invalid Entry')
 
-if __name__ == "__main__":            
+
+if __name__ == "__main__":
     dice()
