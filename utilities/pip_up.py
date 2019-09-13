ced72caca4c559df1b66be4ad39bd8e4a48f1564
mcmxl22 <mcmxl@reagan.com>
2019-09-13 13:40:10 -0600

    Minor changes.

--- utilities/pip_up.py
+++ utilities/pip_up.py
@@ -11,18 +11,18 @@ import os
 
 def pkg_update():
     """Update pip packages."""
-    options = ['Yes', 'Exit']
+    options = ["Yes", "Exit"]
     numli.addnum(options)
-    choice = input('Check for updates? ')
+    choice = input("Check for updates? ")
 
-    if choice in '1':
-        print('Checking for updates.')
-        results = os.system('pip list -o')
+    if choice in "1":
+        print("Checking for updates.")
+        results = os.system("pip list -o")
 
-        os.system('pip-review --local --interactive')
+        os.system("pip-review --local --interactive")
         pkg_update()
 
-    elif choice in '2':
+    elif choice in "2":
         raise SystemExit
 
     else:
