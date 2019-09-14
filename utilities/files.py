f55dd61504bd240bd01926f1d9fb66aedeea678d
mcmxl22 <mcmxl@hotmail.com>
2019-09-13 20:24:34 -0600

    Minor changes.

--- utilities/files.py
+++ utilities/files.py
@@ -10,44 +10,44 @@ import os.path
 
 def create_files(file_name):
     """Create a file and/or confirm it."""
-    file_name = input('Enter file name. ')
+    file_name = input("Enter file name. ")
 
     if os.path.exists(file_name):
-        print(f'{file_name} already exists!')
+        print(f"{file_name} already exists!")
 
     else:
-        with open(file_name, 'w+'):
+        with open(file_name, "w+"):
 
             if os.path.exists(file_name):
-                print(f'{file_name} created!')
+                print(f"{file_name} created!")
 
 
 def delete_file(file_name):
     """Delete files."""
-    file_name = input('Enter file to be deleted: ')
-    confirm_file = input(f'Are you sure you want to delete {file_name}? ')
+    file_name = input("Enter file to be deleted: ")
+    confirm_file = input(f"Are you sure you want to delete {file_name}? ")
 
-    if confirm_file is 'y':
+    if confirm_file is "y":
         os.remove(file_name)
-        print(f'{file_name} deleted!')
+        print(f"{file_name} deleted!")
 
 
 def file_options(file_name):
     """Choose what to do with a file."""
     while True:
-        file_options = ['Create file', 'Delete file', 'Exit']
+        file_options = ["Create file", "Delete file", "Exit"]
         numli.addnum(file_options)
-        file_choice = input('Choose an option. ')
+        file_choice = input("Choose an option. ")
 
-        if file_choice in '1':
+        if file_choice in "1":
             create_files(file_name)
 
-        elif file_choice in '2':
+        elif file_choice in "2":
             delete_file(file_name)
 
-        elif file_choice in '3':
+        elif file_choice in "3":
             raise SystemExit
 
 
 if __name__ == "__main__":
-    file_options('file_name')
+    file_options("file_name")
