6f9054603833f0b45b015cd1cefff2ee27cd0946
mcmxl22 <mcmxl@reagan.com>
2019-09-16 13:23:50 -0600

    No longer prints a list.

---
+++ fun/first_letter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+first_letter version 1
+Python 3.7
+"""
+
+
+def letter_switch():
+    """Switch first letter of two words."""
+    # Get words.
+    get_words = input("Enter two words. ")
+
+    # Convert words to a list.
+    word_list = list(get_words)
+
+    # Find the space.
+    space = word_list.index(' ')
+
+    # Find first letter of the first word.
+    first = word_list[0:1]
+
+    # Remove first letter of each word.
+    del word_list[0]
+    del word_list[space]
+
+    # Re-insert switched letters.
+    word_list.insert(space, ''.join(first))
+    word_list.insert(0, get_words[space + 1])
+
+    # Print result.
+    result = ''.join(word_list)
+    print(result)
+
+
+if __name__ == "__main__":
+    letter_switch()
