d96a351c61ef7272a906b89433869fb36b826348
mcmxl22 <mcmxl@hotmail.com>
2019-11-17 21:48:36 -0700

    Minor changes.

--- fun/hangman.py
+++ fun/hangman.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Hangman version 3
+Hangman version 3.1
 Python 3.8
 Requires a .txt file with a list of words.
 I used:
@@ -12,11 +12,13 @@ import random
 
 
 def get_word():
-    with open("words_alpha.txt", "r") as f:
-        lines = f.readlines()
-        w = random.choice(lines)
-    word = w.rstrip()  # Removes \n character.
+    """Get a random word."""
+    with open("words_alpha.txt", "r") as file:
+        lines = file.readlines()
+        word_select = random.choice(lines)
+    word = word_select.rstrip()  # Removes \n character.
     return word
+
 
 def main():
     """Hangman game"""
@@ -24,16 +26,16 @@ def main():
             This is a hangman game. It tells you how many letters are in the word.
             You have 2 guesses for each letter in the word. It will fill in the blanks
             when you guess a correct letter. It keeps track of wrong guesses and warns
-            you of duplicates.
+            you of duplicate guesses.
           """)
 
     word_item = get_word()
     letter_list = list(word_item)
-    guesses = ((l := len(letter_list)) * 2)
+    guesses = ((list_len := len(letter_list)) * 2)
     attempts = 0
     incorrect_letters = []
     spaces = ["-" for letter in word_item]
-    print(f"There are {l} letters in the word.")
+    print(f"There are {list_len} letters in the word.")
 
     while attempts < guesses:
         guess_letter = input("Guess a letter. ")
@@ -42,16 +44,12 @@ def main():
 
         if guess_letter in letter_list:
             start = 0
-            while True:
-                try:
-                    locate = letter_list.index(guess_letter, start)
-                    spaces[locate] = guess_letter
-                    start = locate + 1
-                    print(f"Letter {locate + 1} is: {guess_letter}")
-                    print(remaining)
-                    print("".join(spaces))
-                except:
-                    break
+            for letter in range(letter_list.count(guess_letter)):
+                locate = letter_list.index(guess_letter, start)
+                spaces[locate] = guess_letter
+                start = locate + 1
+                print(remaining)
+                print("".join(spaces))
 
             while attempts >= guesses / 2:
                 guess_word = input("Guess the word, y/n? ")
@@ -65,6 +63,7 @@ def main():
                         break
                 else:
                     break
+
         else:
             if guess_letter not in incorrect_letters:
                 incorrect_letters.append(guess_letter)
@@ -75,6 +74,7 @@ def main():
 
     else:
         print(f"You're hung! The word was {word_item}.")
+        raise SystemExit
 
 
 if __name__ == "__main__":
