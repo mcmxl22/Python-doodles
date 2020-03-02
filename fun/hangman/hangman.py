c636797587dc4accbec6d112839ac8e563919b38
Micah M <mcmxl@reagan.com>
2020-03-02 11:43:09 -0700

    improvements
    Clears the screen after most guesses to keep things looking simple.

--- fun/hangman/hangman.py
+++ fun/hangman/hangman.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Hangman version 3.8
+Hangman version 3.4
 Python 3.8
 Requires a .txt file with a list of words.
 I used:
@@ -14,7 +14,7 @@ from sys import exit, platform
 
 def clear_screen():
     """Clear the screen."""
-    if platform == "win32":
+    if platform in "win32":
         system("cls")
 
     else:
@@ -23,7 +23,8 @@ def clear_screen():
 
 def get_word():
     """Get a random word."""
-    with open("C:/Users/mcmxl/mystuff/hangman/resources/words_alpha.txt", "r") as file:
+    word_list_url = "C:/Users/mcmxl/mystuff/hangman/resources/words_alpha.txt"
+    with open(word_list_url, "r") as file:
         lines = file.readlines()
         word_select = choice(lines)
     word = word_select.rstrip()  # Removes \n character.
@@ -32,13 +33,14 @@ def get_word():
 
 def main():
     """Hangman game"""
+    clear_screen()
     print(
         """
-            This is a hangman game. It tells you how many letters are in the word.
-            You have 2 guesses for each letter in the word. It will fill in the blanks
-            when you guess a correct letter. It keeps track of wrong guesses and warns
-            you of duplicate guesses. You can type exit or quit to end the game at any time.
-          """
+        This is a hangman game. It tells you how many letters are in the word.
+        You have 2 guesses for each letter. It will fill in the blanks when
+        you guess a correct letter. It keeps track of wrong guesses and warns
+        you of duplicate guesses. You can type exit or quit to end the game at any time.
+        """
     )
 
     word_item = get_word()
@@ -63,9 +65,11 @@ def main():
             exit(0)
 
         elif len(guess_letter) > 1:
+            clear_screen
             print("You can only guess a single letter!")
 
         elif guess_letter in number_list:
+            clear_screen()
             print("You must guess a letter!")
 
         else:
@@ -74,9 +78,9 @@ def main():
 
             else:
                 print(f"You already guessed {guess_letter}!")
-
+            clear_screen()
             print(f"There is no {guess_letter}!")
-        
+
         attempts += 1
 
         if guess_letter in letter_list:
@@ -91,20 +95,25 @@ def main():
             format_word = "".join(dashes)
 
             if format_word in word_item:
+                clear_screen()
                 print(f"{format_word}\nYou win!")
                 exit(0)
 
             else:
+                clear_screen()
                 print(f"{remaining_guesses}\n{format_word}")
 
             while attempts >= guesses / 2:
                 guess_word = input("Guess the word, y/n? ")
 
                 if guess_word in "y":
+                    clear_screen()
+                    print(format_word)
                     guess = input("What is the word? ")
 
                     if guess in word_item:
-                        print("Correct! You win!")
+                        clear_screen()
+                        print(f"{guess}\nYou win!")
                         exit(0)
 
                     else:
