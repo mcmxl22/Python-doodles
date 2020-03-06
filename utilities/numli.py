3f638d546046367bad389d09c57490e90b314560
Micah M <mcmxl@reagan.com>
2020-03-05 23:34:54 -0700

    Update numli.py

--- utilities/numli.py
+++ utilities/numli.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-numli Version 1
+numli Version 1.1
 Python 3.7
 """
 
 
-def addnum(num):
+def add_numbers(num):
     """Add numbers to a list"""
     for c, value in enumerate(num, 1):
         print(c, value)
 
 if __name__ == "__main__":
-    addnum(num)
+    add_numbers("num")
