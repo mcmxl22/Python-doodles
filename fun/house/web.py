6f13b22f91ac5bbac010287e94d56c521f0db06a
Micah M <mcmxl@reagan.com>
2020-04-17 13:48:02 -0600

    Update web.py

--- fun/house/web.py
+++ fun/house/web.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """
-Web version 1.4
+Web version 1.5
 Python 3.7
 """
 
-import webbrowser
+from webbrowser import open
 
 
 def web_site(site_name):
     """website"""
     site_name = input("Type site name: ")
     url = f"https://www.{site_name}.com"
-    webbrowser.open(url)
+    open(url)
 
 
 if __name__ == "__main__":
