6fd0dbad6270a479e2a26602359e10ab67600f24
Micah M <mcmxl@reagan.com>
2020-05-03 14:11:28 -0600

    Add files via upload

---
+++ fun/hangman/get_path.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+get_word version 1
+Python 3.8
+"""
+
+import os
+
+
+def get_path(path):
+    os.chdir("resources")
+    path = os.path.abspath("words_alpha.txt")
+    return path
+
+
+if __name__ == "__main__":
+    exit(get_path("path"))
