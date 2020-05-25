9460da1625b1216a34972521fb9ade3f23115da6
Micah M <mcmxl@reagan.com>
2020-05-24 22:08:45 -0600

    Create clear_and_exit.py

---
+++ utilities/clear_and_exit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+clear_and_exit version 1
+Python 3.8
+Requires: clear_screen.py
+"""
+
+from clear_screen import clear_screen
+
+
+def clear_and_exit():
+    exit(clear_screen())
+
+
+if __name__ == "__main__":
+    exit(clear_screen())
