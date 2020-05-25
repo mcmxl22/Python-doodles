3cec31131242c3e90c0b0a0cf8705f40dfe2332c
Micah M <mcmxl@reagan.com>
2020-05-25 15:31:53 -0600

    Minor improvements.
    Replaced recursive call with while-loop.
    Changed imports.

--- utilities/pip_up.py
+++ utilities/pip_up.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
 """
-pip_up version 1.1
+pip_up version 1.2
 requires: numli.py
 Python 3.7
 """
 
-from numli import add_numbers
+from numli import addnum
 from os import system
 
 
 def pkg_update():
     """Update pip packages."""
-    options = ["Yes", "Exit"]
-    add_numbers(options)
-    choice = input("Check for updates? ")
+    while True:
+        options = ["Yes", "Exit"]
+        addnum(options)
 
-    if choice in "1":
-        print("Checking for updates.")
-        results = system("pip list -o")
+        choice = input("Check for updates? ")
 
-        system("pip-review --local --interactive")
-        pkg_update()
-
-    elif choice in "2":
-        exit(0)
-
-    else:
-        pkg_update()
+        if choice in "1":
+            print("Checking for updates.")
+            results = system("pip list -o")
+            system("pip-review --local --interactive")
+            pkg_update()
+        else:
+            exit(0)
 
 
 if __name__ == "__main__":
