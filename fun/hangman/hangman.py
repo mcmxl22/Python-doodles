ff1a213bf20701f1f3a543fea082eedc246a4014
Micah M <mcmxl@reagan.com>
2020-05-24 22:20:03 -0600

    Refactoring!
    Added functions clear_and_exit() and clear_and_print().
    Used added functions where applicable.

--- fun/hangman/hangman.py
+++ fun/hangman/hangman.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Hangman version 4.3
+Hangman version 4.4
 Python 3.8
 Requires: get_word.py, clear_screen.py
 """
@@ -9,16 +9,25 @@ from clear_screen import clear_screen
 from get_word import get_word
 
 
+def clear_and_exit():
+    exit(clear_screen())
+
+
+def clear_and_print(text):
+    clear_screen()
+    print(text)
+
+
 def main():
     """Hangman game"""
-    clear_screen()
-    print(
-    """
-    This is a hangman game. It tells you how many letters are in the word.
-    You have 2 guesses for each letter. It will fill in the blanks when
-    you guess a correct letter. It keeps track of wrong guesses and warns
-    you of duplicate guesses. You can type exit or quit to end the game at any time.
-    """
+
+    clear_and_print(
+        """
+This is a hangman game. It tells you how many letters are in the word.
+You have 2 guesses for each letter. It will fill in the blanks when
+you guess a correct letter. It keeps track of wrong guesses and warns
+you of duplicate guesses. You can type exit or quit to end the game at any time.
+"""
     )
 
     word_item = get_word()
@@ -31,30 +40,22 @@ def main():
 
     while attempts < guesses:
         try:
-            guess_letter = input("Guess a letter. ").lower()
-
+            guess_letter = input("Guess a letter. ")
         except KeyboardInterrupt:
-            clear_screen()
-            exit(0)
+            clear_and_exit()
 
-        if guess_letter in ["exit", "quit"]:
-            clear_screen()
-            exit(0)
-
-        elif not guess_letter.isalpha() or len(guess_letter) > 1:
-            clear_screen()
-            print("Please enter a single letter!")
-
+        if guess_letter.lower() in ["exit", "quit"]:
+            clear_and_exit()
+        elif not guess_letter.isalpha():
+            clear_and_print("Please enter a letter!")
+        elif len(guess_letter) > 1:
+            clear_and_print("Please enter a single letter!")
         else:
             if guess_letter not in incorrect_letters:
                 incorrect_letters.append(guess_letter)
-                clear_screen()
-                print(f"There is no {guess_letter}!")
-
+                clear_and_print(f"There is no {guess_letter}!")
             elif guess_letter in incorrect_letters:
-                clear_screen()
-                print(f"You already guessed {guess_letter}!")
-
+                clear_and_print(f"You already guessed {guess_letter}!")
             else:
                 break
 
@@ -74,40 +75,28 @@ def main():
             win = f"You won in {attempts} guesses!"
 
             if format_word in word_item:
-                clear_screen()
-                print(f"{format_word}\n{win}")
-                exit(0)
-
+                exit(clear_and_print(f"{format_word}\n{win}"))
             else:
-                clear_screen()
-                print(f"{remaining_guesses}\n{format_word}")
+                clear_and_print(f"{remaining_guesses}\n{format_word}")
 
             while attempts >= guesses / 2:
                 guess_word = input("Guess the word? y/n ").lower()
 
-                if guess_word in "y":
-                    clear_screen()
-                    print(format_word)
+                if guess_word.lower() in "y":
+                    clear_and_print(format_word)
                     guess = input("What is the word? ").lower()
 
                     if guess.lower() in word_item:
-                        clear_screen()
-                        print(f"{guess}\n{win}")
-                        exit(0)
-
+                        exit(clear_and_print(f"{guess}\n{win}"))
                     else:
-                        clear_screen()
-                        print(f"{guess} is incorrect!\n{format_word}")
+                        clear_and_print(f"{guess} is incorrect!\n{format_word}")
                         break
-
                 else:
-                    clear_screen()
-                    print(format_word)
+                    clear_and_print(format_word)
                     break
 
     else:
-        print(f"You're hung! The word was {word_item}.")
-        exit(0)
+        exit(f"You're hung! The word was {word_item}.")
 
 
 if __name__ == "__main__":
