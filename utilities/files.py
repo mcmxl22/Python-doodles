7c6403e471664c49645ee1962dfe316eb40e1fbb
Micah M <mcmxl@reagan.com>
2020-05-26 01:21:58 -0600

    Minor changes.

--- utilities/files.py
+++ utilities/files.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-files Version 1.6
+files Version 1.7
 requires: numli.py
 Python 3.7
 """
-from numli import add_numbers
+
 from os import path, remove
+from numli import add_numbers
 
 
 def create_files(file_name):
@@ -20,6 +21,8 @@ def create_files(file_name):
 
             if path.exists(file_name):
                 print(f"{file_name} created!")
+            else:
+                return
 
 
 def delete_file(file_name):
@@ -27,25 +30,25 @@ def delete_file(file_name):
     file_name = input("Enter file to be deleted: ")
     confirm_file = input(f"Are you sure you want to delete {file_name}? ")
 
-    if confirm_file is "y":
+    if confirm_file in "y":
         remove(file_name)
         print(f"{file_name} deleted!")
+    else:
+        exit(0)
 
 
 def file_options(file_name):
     """Choose what to do with a file."""
     while True:
-        file_options = ["Create file", "Delete file", "Exit"]
-        add_numbers(file_options)
+        options = ["Create file", "Delete file", "Exit"]
+        add_numbers(options)
         file_choice = input("Choose an option. ")
 
         if file_choice in "1":
             create_files(file_name)
-
         elif file_choice in "2":
             delete_file(file_name)
-
-        elif file_choice in "3":
+        else:
             exit(0)
 
 
