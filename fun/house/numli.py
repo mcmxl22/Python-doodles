4d6d9beda6c670810ffd4ff72bc0915296ac2b69
Micah M <mcmxl@reagan.com>
2020-07-16 13:34:18 -0600

    Update numli.py

--- fun/house/numli.py
+++ fun/house/numli.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """
-numli Version 1
+numli Version 1.1
 Python 3.7
 """
 
+import sys
 
-def addnum(num):
+
+def add_numbers(num):
     """Add numbers to a list"""
     for c, value in enumerate(num, 1):
         print(c, value)
 
+
 if __name__ == "__main__":
-    addnum(num)
+    sys.exit(add_numbers("num"))
