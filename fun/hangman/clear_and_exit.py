82b7a5fc6d1fb67504e95b61bc8d45e2c4ece136
Micah M <mcmxl@reagan.com>
2021-04-18 20:35:28 -0600

    Add files via upload

---
+++ fun/hangman/clear_and_exit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+clear_and_exit version 1
+Python 3.8
+"""
+
+from os import system
+from sys import platform
+
+def clear_and_exit():
+    """Clear the screen."""
+    if platform in "win32":
+        system("cls")
+        exit(0)
+
+    else:
+        system("clear")
+        exit(0)
+
+if __name__ == "__main__":
+    exit(clear_and_exit())
