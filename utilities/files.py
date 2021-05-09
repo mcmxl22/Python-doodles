3d39bffbc3c61d4eed9d959739e4151ffa528aef
Micah M <mcmxl@reagan.com>
2021-05-09 12:06:10 -0600

    Add classes

--- utilities/files.py
+++ utilities/files.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-files Version 1.7
+files Version 1.8
 requires: numli.py
 Python 3.7
 """
@@ -11,48 +11,59 @@ from os import path, remove
 from numli import add_numbers
 
 
-def create_files(file_name):
-    """Create a file and/or confirm it."""
-    file_name = input("Enter file name. ")
+class Options:
+    def __init__(self):
+        pass
 
-    if path.exists(file_name):
-        print(f"{file_name} already exists!")
+    def file_options(self):
+        """Choose what to do with a file."""
+        while True:
+            file_options = ["Create file", "Delete file", "Exit"]
+            add_numbers(file_options)
+            file_choice = input("Choose an option. ")
 
-    else:
-        with open(file_name, "w+"):
-
-            if path.exists(file_name):
-                print(f"{file_name} created!")
+            if file_choice in "1":
+                Files.create_files("")
+            elif file_choice in "2":
+                Files.delete_file("")
             else:
-                return
+                exit(0)
 
 
-def delete_file(file_name):
-    """Delete files."""
-    file_name = input("Enter file to be deleted: ")
-    confirm_file = input(f"Are you sure you want to delete {file_name}? ")
+class Files:
+    def __init__(self, name):
+        self.name = name
 
-    if confirm_file in "y":
-        remove(file_name)
-        print(f"{file_name} deleted!")
-    else:
-        sys.exit(0)
+    def create_files(self):
+        """Create a file and/or confirm it."""
+        name = input("Enter file name. ")
 
+        if path.exists(name):
+            print(f"{name} already exists!")
 
-def file_options(file_name):
-    """Choose what to do with a file."""
-    while True:
-        options = ["Create file", "Delete file", "Exit"]
-        add_numbers(options)
-        file_choice = input("Choose an option. ")
-
-        if file_choice in "1":
-            create_files(file_name)
-        elif file_choice in "2":
-            delete_file(file_name)
         else:
-            exit(0)
+            with open(name, "w+"):
+
+                if path.exists(name):
+                    print(f"{name} created!")
+                else:
+                    return
+
+    def delete_file(self):
+        """Delete files."""
+        file_name = input("Enter file to be deleted: ")
+        confirm_file = input(f"Are you sure you want to delete {file_name}? ")
+
+        if confirm_file in "y":
+            remove(file_name)
+            print(f"{file_name} deleted!")
+        else:
+            sys.exit(0)
+
+
+def main():
+    Options.file_options("")
 
 
 if __name__ == "__main__":
-    sys.exit(file_options("file_name"))
+    main()
