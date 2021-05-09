59c385a529b1db5f52330a73092143871d6e4d46
Micah M <mcmxl@reagan.com>
2021-05-08 20:48:47 -0600

    Added class
    Add class Web_site
    change web_site() to visit()

--- fun/house/web.py
+++ fun/house/web.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 """
-Web version 1.5
+Web version 1.6
 Python 3.7
 """
 
 from webbrowser import open
 
 
-def web_site(site_name):
-    """website"""
-    site_name = input("Type site name: ")
-    url = f"https://www.{site_name}.com"
-    open(url)
+class Web_site:
+    def __init__(self, url):
+        self.url = url
+
+
+    def visit(self):
+        """website"""
+        site_name = input("Type site name: ")
+        url = f"https://www.{site_name}.com"
+        open(url)
+
+address = Web_site.visit("site")
 
 
 if __name__ == "__main__":
-    web_site("site_name")
+    address
