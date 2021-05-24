6508b89a6e1566d9d7fb4d837e3757b4e93fe460
Micah M <mcmxl@reagan.com>
2021-05-23 20:33:03 -0600

    Remove classes

--- fun/hangman/hangman.py
+++ fun/hangman/hangman.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 """
-Hangman version 5.2
+Hangman version 5.3
 Python 3.8
-Requires: get_word.py, clear_screen.py, clear_and_exit.py
+Requires: get_word.py, clear_and_exit.py
 """
 
 import sys
-from sys import platform
+from os import system
+from get_word import get_word
+from clear_and_exit import clear_and_exit
 
 
 # Check Python version.
@@ -17,34 +19,26 @@ else:
     pass
 
 
-from clear_and_exit import clear_and_exit
-from get_word import get_word
-from os import system
-
-
-class Prompt:
+def prompt(word):
     """Creates custom prompt."""
-    def make_prompt(phrase):
-        enter = input(f"{phrase}")
-        return enter.lower()
+    enter = input(f"{word}")
+    return enter.lower()
 
 
-class Screen:
+def clear_and_print(text):
     """Clears screen and prints something."""
-    def clear_and_print(text):
-        if platform in "win32":
-            system("cls")
-        else:
-            system("clear")
+    if sys.platform in "win32":
+        system("cls")
+    else:
+        system("clear")
 
-        print(text)
+    print(text)
 
 
 def main():
     """Hangman game"""
     word_item = get_word()
-    prompt = Prompt.make_prompt
-    clear_screen = Screen.clear_and_print
+    clear_screen = clear_and_print
     available_guesses = (list_length := len(word_item)) * 2
     attempts = 0
     dashes = ["-" for letter in word_item]  # Replaces letters with dashes.
@@ -70,55 +64,38 @@ The word has {list_length} letters."""
             print(error)
             continue
 
-
-        if guess_letter in ["exit", "quit"]:
-            clear_and_exit()
-        elif guess_letter not in incorrect_letters:
+        if guess_letter not in incorrect_letters:
             incorrect_letters.append(guess_letter)
             clear_screen(f"There is no {guess_letter}!")
-        elif guess_letter in incorrect_letters:
-            clear_screen(f"You already guessed {guess_letter}!")
         else:
-            break
+            clear_screen(f"You already guessed {guess_letter}!")
 
         attempts += 1
 
         if guess_letter in word_item:
             clear_screen(f"{guess_letter} is in the word!")
-            remaining_guesses = f"Guesses remaining: {available_guesses - attempts}"
 
             # Puts guessed letters back in the word.
+            # pylint: disable=unused-variable
             start = 0
             for letter in range(list(word_item).count(guess_letter)):
                 locate_letter = list(word_item).index(guess_letter, start)
                 dashes[locate_letter] = guess_letter
                 start = locate_letter + 1
+            # pylint: enable=unused-variable
 
-            format_word = "".join(dashes)
-            win = f"You win! Score: {available_guesses - attempts}"
-
-            if format_word in word_item:
-                sys.exit(clear_screen(f"{format_word}\n{win}"))
+            if (format_word := "".join(dashes)) in word_item:
+                sys.exit(clear_screen(f"{format_word}\nYou win!"))
             else:
-                print((remaining := f"{remaining_guesses}\n{format_word}"))
+                print(
+                    (
+                        f"""Guesses remaining: {available_guesses - attempts}
+                            \r{format_word}"""
+                    )
+                )
 
-            while attempts >= available_guesses / 2:
-                guess_word = prompt("Guess the word? y/n")
-
-                if guess_word in "y":
-                    clear_screen(format_word)
-                    guess = prompt("What is the word?")
-
-                    if guess in word_item:
-                        sys.exit(Screen.clear_and_print(f"{guess}\n{win}"))
-                    else:
-                        clear_screen(f"{guess} is incorrect!")
-                        print(format_word)
-                        break
-
-                else:
-                    Screen.clear_and_print(remaining)
-                    break
+        else:
+            continue
 
     else:
         sys.exit(f"You're hung! The word was {word_item}.")
