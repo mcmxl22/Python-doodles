373eb55ae3eed1f6845d6386671bd99add46f4c1
Micah M <mcmxl@reagan.com>
2021-05-23 18:13:12 -0600

    Add Classes and assert statements
    Add classes Prompt and Screen. 
    Replace some if/else with assert. (lines 65, 66)

--- fun/hangman/hangman.py
+++ fun/hangman/hangman.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+
 """
-Hangman version 5.1
+Hangman version 5.2
 Python 3.8
 Requires: get_word.py, clear_screen.py, clear_and_exit.py
 """
 
 import sys
+from sys import platform
 
 
 # Check Python version.
@@ -16,62 +18,73 @@ else:
 
 
 from clear_and_exit import clear_and_exit
-from clear_screen import clear_screen
 from get_word import get_word
+from os import system
 
 
-def prompt(word):
-    """Prompt"""
-    enter = input(f"{word} ")
-    return enter.lower()
+class Prompt:
+    """Creates custom prompt."""
+    def make_prompt(phrase):
+        enter = input(f"{phrase}")
+        return enter.lower()
 
 
-def clear_and_print(text):
-    """Clear screen and print something."""
-    clear_screen()
-    print(text)
+class Screen:
+    """Clears screen and prints something."""
+    def clear_and_print(text):
+        if platform in "win32":
+            system("cls")
+        else:
+            system("clear")
+
+        print(text)
 
 
 def main():
     """Hangman game"""
     word_item = get_word()
+    prompt = Prompt.make_prompt
+    clear_screen = Screen.clear_and_print
     available_guesses = (list_length := len(word_item)) * 2
     attempts = 0
     dashes = ["-" for letter in word_item]  # Replaces letters with dashes.
     incorrect_letters = []
 
-    clear_and_print(
+    clear_screen(
         f"""
 This is a hangman game. It tells you how many letters are in the word.
 You have 2 guesses for each letter. It fills in the blanks when
 you guess a correct letter. It keeps track of guesses and warns
 you of duplicate guesses. Type exit or quit to end the game at any time.
-The word has {list_length} letters.
-"""
+The word has {list_length} letters."""
     )
 
     while attempts < available_guesses:
         try:
-            guess_letter = prompt("Guess a letter.")
+            guess_letter = prompt("Guess a letter: ")
+            assert guess_letter.isalpha(), "Please enter a letter!"
+            assert len(guess_letter) == 1, "Please enter a single letter!"
         except KeyboardInterrupt:
             clear_and_exit()
+        except AssertionError as error:
+            print(error)
+            continue
+
 
         if guess_letter in ["exit", "quit"]:
             clear_and_exit()
-        elif not guess_letter.isalpha() or len(guess_letter) > 1:
-            clear_and_print("Please enter a single letter!")
         elif guess_letter not in incorrect_letters:
             incorrect_letters.append(guess_letter)
-            clear_and_print(f"There is no {guess_letter}!")
+            clear_screen(f"There is no {guess_letter}!")
         elif guess_letter in incorrect_letters:
-            clear_and_print(f"You already guessed {guess_letter}!")
+            clear_screen(f"You already guessed {guess_letter}!")
         else:
             break
 
         attempts += 1
 
         if guess_letter in word_item:
-            clear_and_print(f"{guess_letter} is in the word!")
+            clear_screen(f"{guess_letter} is in the word!")
             remaining_guesses = f"Guesses remaining: {available_guesses - attempts}"
 
             # Puts guessed letters back in the word.
@@ -85,7 +98,7 @@ The word has {list_length} letters.
             win = f"You win! Score: {available_guesses - attempts}"
 
             if format_word in word_item:
-                sys.exit(clear_and_print(f"{format_word}\n{win}"))
+                sys.exit(clear_screen(f"{format_word}\n{win}"))
             else:
                 print((remaining := f"{remaining_guesses}\n{format_word}"))
 
@@ -93,18 +106,18 @@ The word has {list_length} letters.
                 guess_word = prompt("Guess the word? y/n")
 
                 if guess_word in "y":
-                    clear_and_print(format_word)
+                    clear_screen(format_word)
                     guess = prompt("What is the word?")
 
                     if guess in word_item:
-                        sys.exit(clear_and_print(f"{guess}\n{win}"))
+                        sys.exit(Screen.clear_and_print(f"{guess}\n{win}"))
                     else:
-                        clear_and_print(f"{guess} is incorrect!")
+                        clear_screen(f"{guess} is incorrect!")
                         print(format_word)
                         break
 
                 else:
-                    clear_and_print(remaining)
+                    Screen.clear_and_print(remaining)
                     break
 
     else:
