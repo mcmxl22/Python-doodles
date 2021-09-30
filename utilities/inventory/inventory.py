ed492105d0d3be00f7c7b77a94f6001be127bd4d
Micah M <mcmxl@reagan.com>
2021-09-30 13:41:02 -0600

    Update inventory.py

--- utilities/inventory/inventory.py
+++ utilities/inventory/inventory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Inventory.py Version 2.3
+Inventory.py Version 2.4
 Requires: numli.py, clear_screen.py
 Python 3.7
 """
@@ -13,6 +13,7 @@ from clear_screen import clear_screen
 from numli import add_numbers
 
 
+
 def create_inventory_file():
     """Create file"""
     name = "inventory.json"
@@ -22,14 +23,6 @@ def create_inventory_file():
             print(f"{name} created!")
         else:
             return
-
-
-def choices() -> list:
-    """Give user a choice of actions."""
-    inventory_actions = ["Add", "Take", "View", "Delete", "Exit"]
-    add_numbers(inventory_actions)
-    action_choice = input("What do you want to do? ")
-    return action_choice
 
 
 def get_data() -> dict:
@@ -45,61 +38,81 @@ def get_data() -> dict:
     return existing_items
 
 
-def add_inventory() -> str:
-    """Add items to inventory."""
-    item = get_data()
+class Menu:
 
-    try:
-        add_item = input("Enter item: ")
-        quantity = int(input("Enter quantity: "))
-
-        if add_item in item:
-            item[add_item] += quantity
-        else:
-            item[add_item] = quantity
-
-    except ValueError as e:
-        print(e)
-
-    return item
+    def list_choices() -> list:
+        """Give user a choice of actions."""
+        inventory_actions = ["Add", "Take", "View", "Delete", "Exit"]
+        add_numbers(inventory_actions)
+        action_choice = input("What do you want to do? ")
+        return action_choice
 
 
-def delete_item():
-    """Delete items from inventory."""
-    item = get_data()
+class Inventory:
 
-    try:
+    def add_inventory() -> str:
+        """Add items to inventory."""
+        item = get_data()
+
+        try:
+            add_item = input("Enter item: ")
+            quantity = int(input("Enter quantity: "))
+
+            if add_item in item:
+                item[add_item] += quantity
+            else:
+                item[add_item] = quantity
+
+        except ValueError as e:
+            print(e)
+
+        return item
+
+
+    def delete_item():
+        """Delete items from inventory."""
+        clear_screen()
+        item = get_data()
+
+        try:
+            if item == {}:
+                print("Nothing to delete.")
+                pass
+
+            else:
+                delete = input("Enter item to delete: ")
+                item.pop(delete)
+                print(f"{delete} deleted!\n")
+                with open("inventory.json", "w") as file:
+                    json.dump(item, file, indent=4)
+
+        except KeyError as e:
+            print(e)
+
+        return item
+
+
+    def take_items():
+        """Take items from inventory."""
+        item = get_data()
         if item == {}:
-            print("Nothing to delete.")
-            pass
+            print("No inventory available!\n")
+
         else:
-            delete = input("Enter item to delete: ")
-            item.pop(delete)
-            print(f"{delete} deleted!")
+            try:
+                take = input("What did you take? ")
+                deduct = int(input(f"How many {take}? "))
 
-    except KeyError as e:
-        print(e)
+                if take in item:
+                    item[take] -= deduct
+                    print(f"{item[take]} {take} left!\n")
+                else:
+                    print(f"{take} doesn't exist.\n")
 
-    return item
+            except ValueError as e:
+                print(e)
 
-
-def take_items():
-    """Take items from inventory."""
-    item = get_data()
-
-    try:
-        take = input("What did you take? ")
-        deduct = int(input(f"How many {take}? "))
-
-        if take in item:
-            item[take] -= deduct
-        else:
-            print(f"{take} doesn't exist.")
-
-    except ValueError as e:
-        print(e)
-
-    return item
+        return item
 
 
 def main():
@@ -112,39 +125,37 @@ def main():
             create_inventory_file()
 
         # Get user's choice from choices().
-        choice = choices()
+        choice = Menu.list_choices()
 
         # Process user's choice.
         if choice in "1":
             # Add items to inventory.
             clear_screen()
             with open("inventory.json", "r+") as file:
-                add = add_inventory()
+                add = Inventory.add_inventory()
                 json.dump(add, file, indent=4)
 
         elif choice in "2":
             # Take items from inventory.
             clear_screen()
-            taken_items = take_items()
+            taken_items = Inventory.take_items()
             with open("inventory.json", "w") as file:
                 json.dump(taken_items, file, indent=4)
 
         elif choice in "3":
-            # View all inventory.
             clear_screen()
             view_items = get_data()
             if view_items == {}:
-                print("No inventory available")
+                print("No inventory available!\n")
             else:
                 for item, view in view_items.items():
-                    print(item, "=", view)
+                    if view < 1:
+                        print("No inventory available!\n")
+                    else:
+                        print(item, "=", view)
 
         elif choice in "4":
-            # Delete items from inventory.
-            clear_screen()
-            delete = delete_item()
-            with open("inventory.json", "w") as file:
-                json.dump(delete, file, indent=4)
+            Inventory.delete_item()
 
         else:
             clear_screen()
@@ -152,4 +163,5 @@ def main():
 
 
 if __name__ == "__main__":
+    clear_screen()
     exit(main())
