edd391440050a0eb461d1f322d7a32f5db4c86e9
Micah M <mcmxl@reagan.com>
2021-12-10 11:49:15 -0700

    Update numli.py

--- numli.py
+++ numli.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 """
-numli Version 1
+numli Version 1.2
 Python 3.7
 """
 
 
-def addnum(num):
-    """Add numbers to a list"""
-    for c, value in enumerate(num, 1):
-        print(c, value)
+class Menu:
+    """
+    Prepare a menu. 
+    Example: Menu.list_choices(['Cat','Dog'])
+    Output:
+    1 Cat
+    2 Dog
+    """
+    def add_numbers(num):
+        """Add numbers to the menu list."""
+        for c, value in enumerate(num, 1):
+            print(c, value)
 
 
-if __name__ == "__main__":
-    addnum("num")
+    def list_choices(choose_conversion, **kwargs: list):
+        """Give user a choice of actions."""
+        return Menu.add_numbers(choose_conversion)
