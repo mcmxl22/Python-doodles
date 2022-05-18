dc7b833a1740e3793de7b4dc2797bf30f6c655ee
Micah M <mcmxl@reagan.com>
2022-05-18 14:09:43 -0600

    Added use of class.

--- fun/age100.py
+++ fun/age100.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 """
-age100 version 1.3
-Python 3.7
+age100 version 1.4
+Python 3.10.X
 """
 
 import datetime
 
+class User:
+    def __init__(self):
+        self.name = input('Enter name. ')
+        self.age = int(input('Enter age. '))
 
-def age100():
-    """The year you will be 100"""
-    get_name = input("What's your name? ")
-    get_age = int(input("What's your age? "))
+    def show_name(self):
+        return self.name
+    
+    def show_age(self):
+        return self.age
+
+
+def calculate():
+    user = User()
+    user.show_name()
+    user.show_age()
     current_year = datetime.datetime.now()
-    age_100 = (current_year.year - get_age) + 100
+    age_100 = (int(current_year.year) - user.age) + 100
+    return f'{user.name} will be 100 in {age_100}!'
+    
 
-    if get_age >= 100:
-        print("Congratulations! You're already 100!")
-
-    else:
-        print(f"{get_name}, you will be 100 years old in {age_100}!")
-
-if __name__ == "__main__":
-    age100()
+if __name__ == '__main__':
+    print(calculate())
