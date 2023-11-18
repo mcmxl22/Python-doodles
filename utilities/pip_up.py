03c7b275570d7735cfb80b7eb8a8504f61152a69
M. McConnaughey <mcmxl@hotmail.com>
2023-11-18 12:21:26 -0700

    Remove numli.py dependency

--- utilities/pip_up.py
+++ utilities/pip_up.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 
 """
-pip_up version 1.6
-requires: numli.py
+pip_up version 1.7
 Python 3.7
 """
 
 from subprocess import run
-from numli import add_numbers
 
 
 def pkg_update():
     """Update pip packages."""
     while True:
-        options = ["Yes", "Exit"]
-        add_numbers(options)
+        print("1 Yes")
+        print("2 Exit")
 
         choice = input("Check for updates? ")
 
         if choice in "1":
-            print("Checking for updates.")
+            print("Checking for updates...")
             run("pip-review --local --interactive", check=True)
         else:
-            exit(0)
+            exit()
 
 
 if __name__ == "__main__":
-    exit(pkg_update())
+    pkg_update()
