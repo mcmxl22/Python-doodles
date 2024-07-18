4bc94864d20360dd87cb79f7778da036835d41bc
M. McConnaughey <mcmxl@hotmail.com>
2024-07-18 10:03:02 -0600

    Update get_word.py

--- fun/hangman/get_word.py
+++ fun/hangman/get_word.py
@@ -3,24 +3,18 @@
 get_word version 1.2
 Python 3.8
 Requires get_path.py
-Requires a .txt file with a list of words.
-I used:
-https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-long.txt
 """
 
 from get_path import Path
 from random import choice
 
 
-def get_word() -> str:
-    """Get a random word."""
-    file_path = Path.get_path("")
-    with open(file_path, "r") as file:
-        lines = file.readlines()
-        word_select = choice(lines)
-    word = word_select.rstrip()  # Removes \n character.
-    return word
+def get_word(file_path: str) -> str:
+    """Get a random word from file."""
+    with open(file_path) as f:
+        words = f.read().splitlines()
+    return random.choice(words)
 
 
 if __name__ == "__main__":
-    exit(get_word())
+    get_word()
